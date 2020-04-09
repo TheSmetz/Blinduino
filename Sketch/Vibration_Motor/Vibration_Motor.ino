01b219270fab2db289121b3d17e732b2a7dde5e5
TheSmetz <44972483+TheSmetz@users.noreply.github.com>
2020-04-09 16:21:29 +0200

    Sketch ARDUINO Smetz INGEGNERE

---
+++ Sketch/Vibration_Motor/Vibration_Motor.ino
@@ -0,0 +1,30 @@
+#define vibrationOut 8
+#define distanceMeterEcho 9
+#define distanceMeterTrigger 10
+
+long timeMeasure; //Time measure taken from distance meter
+long distanceMeasure; //Distance measure taken from distance meter
+int vibrationDuration = 500; //Duration of the vibration done by the motor
+int maxDistance = 400; //Maximum relevant distance
+
+void setup() {
+  pinMode(vibrationOut, OUTPUT);
+  pinMode(distanceMeterEcho, INPUT);
+  pinMode(distanceMeterTrigger, OUTPUT);
+  digitalWrite(distanceMeterTrigger, LOW);
+}
+
+void loop() {
+  digitalWrite(distanceMeterTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(distanceMeterTrigger, LOW);
+  timeMeasure = pulseIn(distanceMeterEcho, HIGH);
+  distanceMeasure = 0.03434*timeMeasure/2;
+
+  if(distanceMeasure < maxDistance) {
+    digitalWrite(vibrationOut, HIGH);
+    delay(vibrationDuration);
+    digitalWrite(vibrationOut, LOW);
+    delay(timeMeasure/20);
+  }
+}
