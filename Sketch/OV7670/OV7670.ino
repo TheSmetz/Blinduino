493330f2cdd067360f28960744114ccfe5a249ea
TheSmetz <matteo.guerrini@studenti.unicam.it>
2020-04-27 14:46:48 +0200

    MVP Smetz is INSANE, 30L assured

--- Sketch/OV7670/OV7670.ino
+++ Sketch/OV7670/OV7670.ino
@@ -1,25 +1,4 @@
-// Pins configurations:
-// SDA/SIOD ---> pin A4 (for Arduino UNO) 
-// SCL/SIOC ---> pin A5 (for Arduino UNO)
-// MCLK/XCLK --> pin 11 (for Arduino UNO)
-// PCLK -------> pin 2
-// VS/VSYNC ---> pin 3
-// HS/HREF ----> pin 8
-// D0 ---------> pin A0
-// D1 ---------> pin A1
-// D2 ---------> pin A2
-// D3 ---------> pin A3
-// D4 ---------> pin 4
-// D5 ---------> pin 5
-// D6 ---------> pin 6
-// D7 ---------> pin 7
-
-#include <Wire.h>
 #define buttonIn 7                    //Switch Pin
-#define vibrationOut 8                //Vibration Pin
-#define distanceMeterEcho 9           //DistanceMeterEcho Pin
-#define distanceMeterTrigger 10       //DistanceMeterTrigger Pin
-#include "Arduino.h"
 #include <SoftwareSerial.h>           //Library about Bluetooth
 #define CAMERA_ADDRESS 0x21
 
@@ -55,10 +34,6 @@
 
 
 bool state = false;                   //Stores the state, changed by the button (on/off)
-long timeMeasure;                     //Time measure taken from distance meter
-long distanceMeasure;                 //Distance measure taken from distance meter
-int vibrationDuration = 500;          //Duration of the vibration done by the motor
-int maxDistance = 400;                //Maximum relevant distance
 SoftwareSerial BTserial(0, 1);        //RX , TX Initiate our BT object. First value is RX pin, second value TX pin
 
 void initializePWMTimer() {
@@ -130,13 +105,8 @@ void captureFrame(unsigned int frameWidth, unsigned int frameHeight) {
 
 void setup() {
   initializePWMTimer();
-  pinMode(vibrationOut, OUTPUT);                //Setup Vibration
-  pinMode(distanceMeterEcho, INPUT);            //Setup Distancemeter Echo
-  pinMode(distanceMeterTrigger, OUTPUT);        //Setup Distancemeter Trigger
   pinMode(buttonIn, INPUT);                     //Setup Button ON OFF
-  digitalWrite(distanceMeterTrigger, LOW);      //Setup Distancemeter Trigger Off
-  BTserial.begin(9600);                         //Set baud rate of HC-05
-  Serial.begin(9600);                           
+  BTserial.begin(9600);                         //Set baud rate of HC-05                  
   Wire.begin();
   Serial.begin(1000000); // the frame capture software communicates with the Arduino at a baud rate of 1MHz
 }
@@ -149,26 +119,12 @@ void loop() {
    if (digitalRead(buttonIn) == HIGH)  //Control the button HIGH or LOW
     {
         state = !state;                                       //Change state of Button in ON or OFF
-        BTserial.print(state);                                //Print the state of the vibrator
+        BTserial.print(state);                                //Print the state of the OV7670
         delay(500);                                           //Delay to use the button
     }
     if (state)    //Control the state
     {
-        digitalWrite(distanceMeterTrigger, HIGH);             //Send a 10us pulse to the trigger
-        delayMicroseconds(10);                                //Delay to wait the pulse
-        digitalWrite(distanceMeterTrigger, LOW);              //Receive pulse
-        timeMeasure = pulseIn(distanceMeterEcho, HIGH);       //Save the pulse
-        distanceMeasure = 0.03434 * timeMeasure / 2;          //Convert the pulse in cm
-        BTserial.print(distanceMeasure);                      //Send to App measure of distance
-        BTserial.print(",");                                  //Separator
-        if (distanceMeasure < maxDistance)                    //Control range of distance
-        {
-            digitalWrite(vibrationOut, HIGH);                 //Activate Vibration
-            delay(vibrationDuration);                         //Duration of vibration
-            digitalWrite(vibrationOut, LOW);                  //Deactivate Vibration 
-            delay(timeMeasure / 20);                          //Set up the time of Vibration based on timeMeasure (avoid 3 If cycles)
-            captureFrame(320, 240); // capture a frame at QVGA resolution (320 x 240)
-        }
-        BTserial.print(";");                                  //Separator
+      captureFrame(320, 240); // capture a frame at QVGA resolution (320 x 240)
     }
+    BTserial.print(";");                                  //Separator
 }
